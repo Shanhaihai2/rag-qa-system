bd7ffd22b1bd462691efda9c03778936ff393d74
shanhaihai2 <1959718879@qq.com>
2026-04-28 14:43:41 +0800

    完成第35个任务：Vue 3 环境搭建，掌握响应式、模板语法、组件通信

---
+++ rag-qa-frontend/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue'
+import HelloMessage from './components/HelloMessage.vue'
+
+const greeting = ref('')
+
+function handleGreet(message) {
+  greeting.value = message
+}
+</script>
+
+<template>
+  <!-- 使用子组件，传入 props -->
+  <HelloMessage name="张三" :age="20" @greet="handleGreet" />
+  <HelloMessage name="李四" :age="22" @greet="handleGreet" />
+
+  <p v-if="greeting" class="greeting-msg">{{ greeting }}</p>
+</template>
---
+++ rag-qa-frontend/src/components/HelloMessage.vue
@@ -0,0 +1,30 @@
+<script setup>
+// 定义父组件可以传入的 props
+const props = defineProps({
+  name: String,
+  age: Number
+})
+
+// 定义可以发射给父组件的事件
+const emit = defineEmits(['greet'])
+
+function sayHello() {
+  emit('greet', `你好，我是 ${props.name}，今年 ${props.age} 岁`)
+}
+</script>
+
+<template>
+  <div class="hello-box">
+    <p>姓名：{{ name }}</p>
+    <p>年龄：{{ age }}</p>
+    <button @click="sayHello">打招呼</button>
+  </div>
+</template>
+
+<style scoped>
+.hello-box {
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin: 10px 0;
+}
+</style>
